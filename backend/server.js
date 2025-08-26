37690fae2d668164213cee1386f7207e190b502c
Ayesha Afzal <ayesha.taisee1@gmail.com>
2025-08-26 22:11:53 +0500

    db connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import userRouter from './routes/userRouter.js'
+import connectDB from './config/mongodb.js'
+import dotenv from "dotenv"
 
 const app = express()
-const port = process.env.PORT ? process.env.PORT : 4000
+const port = process.env.PORT || 4000
+dotenv.config()
+connectDB()
 
 app.use(express.json()) // middleware to parse JSON req bodies
 
